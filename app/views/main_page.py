047c6fa543185959f93123e8bd541e4fdac25e76
MatiFilozof <matibakala@gmail.com>
2021-01-20 18:50:27 +0100

    Update main_page.py

--- app/views/main_page.py
+++ app/views/main_page.py
@@ -3,6 +3,7 @@
 from flask import request, url_for, flash, send_from_directory, jsonify, render_template_string
 from flask import Blueprint, render_template
 
+from wordnet import bag_words, detect_language, rank_categories
 from ..models.forms import BookForm
 
 # When using a Flask app factory we must use a blueprint to avoid needing 'app' for '@app.route'
@@ -14,7 +15,12 @@ def main_page():
     form = BookForm(request.form)
 
     if request.method == 'POST':
+        x = bag_words(form.query.data)
+        categories = rank_categories(x)
+        langs = detect_language(x)
         print(f'Data: {form.query.data}')
+        print(f'Categories ranked: {categories}')
+        print(f'Languages found: {langs}')
         return render_template('pages/book_recommender.html', query="", details="", result=[1, 2, 3])
         # return redirect(url_for('main.example_page'))
 
