ba1b5ac841b5a1ed75760fdbc89e1cbb5b7ab560
MatiFilozof <matibakala@gmail.com>
2021-01-21 19:55:41 +0100

    fixed setness to listness of languages

--- wordnet/detect_hyponym.py
+++ wordnet/detect_hyponym.py
@@ -2,24 +2,24 @@ from nltk.corpus import wordnet as wn
 
 
 def _detect_hyponym(word, condition_synset):
-    # Cannot use natural_language.n.01, because one might want a book in esperanto
-    # Excluding programming languages, though
     comparison_target = wn.synset(condition_synset)
     found = any([comparison_target in synset.closure(lambda x: x.hypernyms(), depth=99) for synset in wn.synsets(word)])
     return found, word
 
 
 def detect_hyponym(bag_of_words, condition_synset):
-    languages = set()
+    hyponyms = set()
     for word in bag_of_words:
-        found, language = _detect_hyponym(word, condition_synset)
+        found, hyponym = _detect_hyponym(word, condition_synset)
         if found:
-            languages.add(language)
-    return languages
+            hyponyms.add(hyponym)
+    return hyponyms
 
 
 def detect_language(bag_of_words):
-    return detect_hyponym(bag_of_words, 'language.n.01') - detect_hyponym(bag_of_words, 'programming_language.n.01')
+    # Cannot use natural_language.n.01, because one might want a book in esperanto
+    # Excluding programming languages, though
+    return list(detect_hyponym(bag_of_words, 'language.n.01') - detect_hyponym(bag_of_words, 'programming_language.n.01'))
 
 
 def detect_genre(bag_of_words):
