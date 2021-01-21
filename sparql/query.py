c8802d9e20fb3f1b71a4f55f885b374c1ba56370
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-01-21 13:39:37 +0100

    Feature #sparql find values by properties

--- sparql/query.py
+++ sparql/query.py
@@ -8,8 +8,8 @@ class QueryExecutor:
     sparql_store = SPARQLStore("http://dbpedia.org/sparql")
 
     @staticmethod
-    def execute(query_string, pretty_print=True, limit=100):
-        query_string += f"\n LIMIT {limit}"
+    def execute(query_string, *args, pretty_print=True, limit=100):
+        query_string = QueryExecutor.prepare_query_string(query_string, *args, limit=limit)
         result = QueryExecutor.sparql_store.query(query_string, initNs=QueryExecutor.namespaces)
         if pretty_print:
             for item in list(result):
@@ -17,16 +17,11 @@ class QueryExecutor:
         return result
 
     @staticmethod
-    def find_by_author(author):
-        q = """
-        select distinct ?s  ?author 
-        where {
-        ?s rdf:type dbo:Book;
-          dbo:author ?author .
-         VALUES ?author { <?1> }
-        }        
-        """.replace("?1", author)
-        return QueryExecutor.execute(q)
+    def prepare_query_string(query_string: str, *args, limit=100):
+        query_string += f"\n LIMIT {limit}"
+        for i, arg in enumerate(args):
+            query_string = query_string.replace(f'?{i+1}', arg)
+        return query_string
 
     @staticmethod
     def find_all_properties(limit=100):
@@ -35,35 +30,39 @@ class QueryExecutor:
         where {
         ?s rdf:type dbo:Book;
           ?prop ?value.
-        }        
+        }
         """
         return QueryExecutor.execute(q, limit=limit)
 
     @staticmethod
-    def show_property_values(property):
+    def find_property_values(prop):
         q = """
-        select distinct ?prop
+        select distinct ?value
         where {
         ?s rdf:type dbo:Book;
           ?1 ?value.
-        }        
-        """.replace("?1", property)
-        return QueryExecutor.execute(q)
-
-    @staticmethod
-    def find_something():
-        q = """
-        select distinct ?s  ?author 
-        where {
-        ?s rdf:type ontology:Book;
-          dbo:author ?author .
-        ?s dbo:lcc ?lcc .
         }
         """
-        return QueryExecutor.execute(q)
+        return QueryExecutor.execute(q, prop)
+
+    @staticmethod
+    def find_by_property(prop, value):
+        q = """
+        select distinct ?s ?value
+        where {
+        ?s rdf:type dbo:Book;
+          ?1 ?value.
+         VALUES ?value { <?2> }        
+        }
+        """
+        return QueryExecutor.execute(q, prop, value)
 
 
 if __name__ == "__main__":
-    QueryExecutor.find_all_properties(limit=200)
-    # QueryExecutor.find_something()
-    # QueryExecutor.find_by_author('http://dbpedia.org/resource/Heinz-Otto_Peitgen')
+    # QueryExecutor.find_all_properties(limit=200)
+    # print("\n\n\n\n\n")
+    # QueryExecutor.find_property_values('dbo:author')
+    # print("\n\n\n\n\n")
+    # QueryExecutor.find_by_property('dbo:author', 'http://dbpedia.org/resource/Heinz-Otto_Peitgen')
+    # print("\n\n\n\n\n")
+    QueryExecutor.find_property_values('dbo:lcc')
