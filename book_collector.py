8c54c84d68bf807cb5f21e1796b89a7766efd82d
MatiFilozof <matibakala@gmail.com>
2021-01-22 19:43:25 +0100

    updated BookCollector for book relevance

--- book_collector.py
+++ book_collector.py
@@ -8,9 +8,9 @@ from wordnet.detect_hyponym import detect_person, detect_work_of_art
 nlp = spacy.load('en_core_web_sm')
 
 
-def score_book_relevance(book):
-    # TODO: make it a little bit better maybe?
-    return 1
+def score_book_relevance(book, category: Category):
+    abstract_doc = nlp(str(book[2]))
+    return category.bag_similarity(bag_words(abstract_doc))
 
 
 def find_by_conditions(category: Category, language: str, people: set, work_of_art: str):
@@ -20,13 +20,14 @@ def find_by_conditions(category: Category, language: str, people: set, work_of_a
     :param category: possible genre properties of book ontology
     :param language: expected language of a book
     :param people: possible people (fictional or not) related to a book
+    :param work_of_art: expected title of a work, possibly a book
     :return: a tuple of book ontology data and its score
     """
     books = QueryExecutor.find_books_by_conditions(category.labels,
                                                    language=language,
                                                    people=people,
                                                    book_name=work_of_art)
-    return [(book, score_book_relevance(book)) for book in books]
+    return [(book, score_book_relevance(book, category)) for book in books]
 
 
 class BookScores:
@@ -42,12 +43,12 @@ class BookScores:
         """
         Collects scores into one number, where greater value means better score.
 
-        :return: a number in range [0, 1]
+        :return: a number in range [0, 10]
         """
         self.final_score = 1
         for score in self.__scores:
             self.final_score *= 1 - score
-        self.final_score = 1 - self.final_score
+        self.final_score = round(10 * (1 - self.final_score), 2)
 
 
 class BookCollector:
@@ -73,16 +74,16 @@ class BookCollector:
         :return: list of book URIs and names sorted by some metric
         """
         languages = list(detect_language(self.bag_of_words))
+        language = languages[0] if len(languages) != 0 else None
         people = detect_person(self.doc)
-        work_of_art = list(detect_work_of_art(self.doc))
+        people = people if len(people) != 0 else None
+        works_of_art = list(detect_work_of_art(self.doc))
+        work_of_art = works_of_art[0] if len(works_of_art) != 0 else None
 
         category_ranking = rank_categories(self.bag_of_words)
         # Select only top 3 categories
         for category, cat_score in category_ranking[:3]:
-            for book, book_score in find_by_conditions(category,
-                                                       languages[0] if len(languages) != 0 else None,
-                                                       people if len(people) != 0 else None,
-                                                       work_of_art[0] if len(work_of_art) != 0 else None):
+            for book, book_score in find_by_conditions(category, language, people, work_of_art):
                 self.assign_score(book, cat_score * book_score)
         self.compute_final_scores()
         return sorted(self.book_scores.values(), key=lambda v: v.final_score, reverse=True)
