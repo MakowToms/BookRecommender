0838c48ea618e730623b92d308ed6ea5ad7b0af9
MakowToms <makowskit2@student.mini.pw.edu.pl>
2021-01-22 19:00:45 +0100

    Feature ~sparql filter book name

--- sparql/query.py
+++ sparql/query.py
@@ -81,7 +81,8 @@ class QueryExecutor:
         return QueryExecutor.execute(q, prop, book, limit=limit)
 
     @staticmethod
-    def find_books_by_conditions(genre_list: list, language: str = None, people: set = None, limit=20):
+    def find_books_by_conditions(genre_list: list, language: str = None,
+                                 people: set = None, book_name: str = None, limit=20):
         q = """
         select distinct ?book ?bookName ?bookAbstract
         where {
@@ -102,12 +103,15 @@ class QueryExecutor:
          VALUES ?value { <http://dbpedia.org/resource/?1> }
          ?2
          ?3
+         ?4
         }
         """
         genre_string = "> <http://dbpedia.org/resource/".join(genre_list)
         language_query = QueryExecutor.generate_language_query_part(language)
         people_query = QueryExecutor.generate_people_query_part(people)
-        return QueryExecutor.execute(q, genre_string, language_query, people_query, limit=limit)
+        book_name_query = QueryExecutor.generate_book_name_query_part(book_name)
+        return QueryExecutor.execute(q, genre_string, language_query, people_query,
+                                     book_name_query, limit=limit)
 
     @staticmethod
     def generate_language_query_part(language: str):
@@ -129,6 +133,16 @@ class QueryExecutor:
               BIND(LCASE(STR(?name)) AS ?name_lower)
               FILTER(?name_lower IN ("*1"))
             """.replace("*1", '", "'.join(people).lower())
+        else:
+            return ""
+
+    @staticmethod
+    def generate_book_name_query_part(book_name: str):
+        if book_name is not None:
+            return """
+            BIND(LCASE(?bookName) AS ?bookNameLower)
+              FILTER(contains(?bookNameLower, "*1"))
+            """.replace("*1", book_name.lower())
         else:
             return ""
 
